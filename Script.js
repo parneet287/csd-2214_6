3999f313b28b530cefcd25ca4f2492cdeb9c9d04
Parneet Kaur <43280045+parneet287@users.noreply.github.com>
2019-01-14 16:53:57 -0500

    week 2-class hands in work

---
+++ Script.js
@@ -0,0 +1,14 @@
+// Name:Parneet Kaur
+//date: January 14,2019
+//Student: c0742279
+//Course: CSD 2214 section 6
+  
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[Name="endDate"]');
+const clock = document.querySelector('clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+//console.log(endDate.value);
